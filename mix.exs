005f9210a7c52f512d996865e73382cb2c5b4df1
Nat Tuck <nat@ferrus.net>
2018-11-30 20:59:35 -0500

    Bump version.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ScriptTools.MixProject do
     Code.require_file("lib/mix/tasks/compile.simple_make.ex")
     [
       app: :script_tools,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
